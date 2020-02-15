60a4c124a943a94486b28e106dccbf01b7aed1df
default <alt.ha-35jm3s7@yopmail.com>
2020-02-15 18:35:18 +0900

    途中まで

---
+++ src/js/PhotoGallery.js
@@ -0,0 +1,7 @@
+let album = [
+  {src: 'img/なみかアメリカ前.jpg', msg: 'ハーブ園'},
+  {src: 'img/好きなパン.JPG', msg: '２'},
+  {src: 'img/ベルリン.jpg', msg: '３'},
+  {src: 'img/デンマーク　スマホ_170704_0235.jpg', msg: '４'},
+  {src: 'img/大学.jpg', msg: '５'},
+];
