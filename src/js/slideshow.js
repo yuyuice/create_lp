2525ebd86db637cee6cadf13a05810d5c5f41a18
default <alt.ha-35jm3s7@yopmail.com>
2020-02-16 16:43:54 +0900

    slideshow途中

---
+++ src/js/slideshow.js
@@ -0,0 +1,11 @@
+//slickの設定を記述する！
+//この書き方で、htmlの読み込みが完了してから処理が実行されるようにこれで囲む。
+$(function(){
+  //スライドに使用する要素を指定するためにセレクタにslideshowを指定する。
+  //プラグインで提供されているメソッド（slick）を使って、スライドショーの設定をしている。
+  $('slideshow').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true
+  });
+});
