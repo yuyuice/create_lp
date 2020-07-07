1f4a60d3e2d23493dc841ac89ddbd8d20c426d4d
yuyuice <yuyuldr@gmail.com>
2020-07-07 14:36:19 +0900

    動かなかったところ修正

--- 20年8月人事考課/qiitaQuiz/qiitaQuiz.js
+++ 20年8月人事考課/qiitaQuiz/qiitaQuiz.js
@@ -1,25 +1,26 @@
 let counterOutput = document.getElementById("counterOutput");
 
-//加算機能
+// 加算
 let plusButton = document.getElementById("plusButton");
+  //arrow function
 let onClickPlusButton = () => {
   counterOutput.value++;
 };
-plusButton.addEventListener('click', onClickPlusButton);
+plusButton.addEventListener("click", onClickPlusButton);
 
-//引き算
+// 引き算
 let minusButton = document.getElementById("minusButton");
 let onClickMinusButton = () => {
   if (counterOutput.value >= 1) {
-    //カウント数が1以上のときだけ引き算
+     // カウント数が1以上のときだけ減算する
     counterOutput.value--;
   }
 };
-minusButton,addEventListener('click', onClickMinusButton);
+minusButton.addEventListener("click", onClickMinusButton);
 
-//リセットボタン
+// リセットボタン
 let resetButton = document.getElementById("resetButton");
 let onClickResetButton = () => {
   counterOutput.value = 0;
 };
-resetButton.addEventListener('click', onClickResetButton);
+resetButton.addEventListener("click", onClickResetButton);
